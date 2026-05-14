ffa9c8483e6434688dcbaae8e47f1b2e821c16ee
Asim Abdulrahman Mohammed Omer <almhbob.iii@gmail.com>
2026-05-14 01:58:28 +0000

    Add platform ownership constants

---
+++ constants/platform.ts
@@ -0,0 +1,18 @@
+export const PLATFORM_OWNER = {
+  nameAr: "فوترة",
+  nameEn: "Fawtara",
+  legalRole: "platform_owner",
+};
+
+export const DEFAULT_TENANT = {
+  id: "wh-cake-chocolate",
+  name: "W&H Cake & Chocolate",
+  slug: "wh-cake-chocolate",
+};
+
+export const TENANT_ROLES = {
+  platformOwner: "مالك منصة فوترة",
+  tenantCompany: "شركة مستأجرة",
+  tenantAdmin: "مدير شركة مستأجرة",
+  tenantEmployee: "موظف داخل شركة مستأجرة",
+};
