c042804d4fa397a2ddd50c5752b081098f3d9b9f
Asim Abdulrahman Mohammed Omer <almhbob.iii@gmail.com>
2026-05-22 03:55:19 +0300

    Add safe developer operations dashboard entry

---
+++ components/DeveloperOperationsDashboardEntry.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { DeveloperOperationsDashboard } from "@/components/DeveloperOperationsDashboard";
+
+export function DeveloperOperationsDashboardEntry() {
+  return <DeveloperOperationsDashboard />;
+}
